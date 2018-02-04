5bf90a7c63da547613991878f2d53c023c5ff6de
paul <mainaccount@Jupitors-iMac.local>
2018-02-04 21:00:58 +0000

    new method added

--- RegExpOne.java
+++ RegExpOne.java
@@ -2,25 +2,53 @@ import java.util.regex.*;
 public class RegExpOne {
 
 	public static void main(String[] args) {
-		String sentence = " Today is sunny. The temperature is 12 degrees celcius.";
+		int zipCode = 12345; // zip code to check
+		String sentence = " John Shanks. New York "+zipCode; // sentence to check
 		
+		regExpFunction("\\s[A-Za-z]{2,50}", sentence); // method call with regular expression language and char params
 		
-		regExpFunction("\\s[A-Za-z]{2,50}", sentence);
+		findZipCodeWithRegExp("\\s\\d{5}", sentence); // zip code method call with param length of 5
 
 	}
 	
+	// regular expression method with 2 params 
 	public static void regExpFunction(String regExp, String string2Check){
 		Pattern checkRegExp = Pattern.compile(regExp);
 		
 		Matcher regExpMatcher = checkRegExp.matcher(string2Check);
 		
+		// while loop to check string param
 		while(regExpMatcher.find()){
-			if(regExpMatcher.group().length() != 0){
+			// trim & print if length > 0
+			if(regExpMatcher.group().length() > 0){
 				System.out.println(regExpMatcher.group().trim());
 			}
+			// print start index and end index
 			System.out.println("Start index: "+regExpMatcher.start());
 			System.out.println("End index: "+regExpMatcher.end());
 		}
 	}
+	
+	// method to check for zip code with 2 params
+	public static void findZipCodeWithRegExp(String regExp, String string2Check){
+		Pattern checkRegExp = Pattern.compile(regExp);
+		
+		Matcher regExpMatcher = checkRegExp.matcher(string2Check);
+		
+		System.out.println("\nZip Code:");
+		
+		// while loop to check for > 0 group()
+		while(regExpMatcher.find()){
+			if(regExpMatcher.group().length() > 0){
+				// trim and print in length > 0
+				System.out.println(regExpMatcher.group().trim());
+			}
+			// print start and end index
+			System.out.println("Start index: "+regExpMatcher.start());
+			System.out.println("End index: "+regExpMatcher.end());
+		}
+	}
+	
+	
 
 }
